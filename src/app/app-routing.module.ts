d9cda0a5865408a35ee3576acb6698346775bb4c
wagblin3 <wagblin3@gmail.com>
2023-02-07 19:59:52 +0100

    mise en forme 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,15 @@ import { ListComponent } from './list/list.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 
 const routes: Routes = [
-
-
-
   {
     path:'', component:ListComponent
   },
-
-
   {
     path:'detail/:id', component:DetailComponent
   },
-  
-  
   {
     path:'login', component:LoginFormComponent
   },
-  
   {
     path:'*', component:LoginFormComponent
   },
